2cba8ea00a578d880f74c276b16321af4128018d
SaranshSharma35 <78153072+SaranshSharma35@users.noreply.github.com>
2021-06-28 17:51:41 +0530

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,16 @@
+class Chain {
+    constructor(bodyA,bodyB) {
+        var options={
+            bodyA:bird.body,
+            bodyB:constrainedLog.body,
+            stiffness:0.04,
+            length:10
+   
+       } 
+       this.chain= Constraint.create(options)
+    World.add(world,this.chain)
+    }
+display(){
+    line (bird.body.position.x,bird.body.position.y,constrainedLog.body.position.x,constrainedLog.body.position.y);
+}
+}
